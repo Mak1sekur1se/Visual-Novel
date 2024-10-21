caf639180df48b96c9f18154c499d51b36672574
Mak1sekur1se <3127242797@qq.com>
2024-10-21 21:08:21 +0800

    dev:新开项目，清理Unity项目版本控制

--- Assets/_Game/Scripts/Core/TextArchitect.cs
+++ Assets/_Game/Scripts/Core/TextArchitect.cs
@@ -1,9 +1,35 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class TextArchitect : MonoBehaviour
+public class TextArchitect 
 {
-    [SerializeField] private TMP_Text text; 
+    private TextMeshProUGUI tmproUi;//2D
+    private TextMeshPro tmproWorld;//3D
+
+    public TMP_Text tmpro => tmproUi != null ? tmproUi : tmproWorld;
+
+    public string currentText => tmpro.text;
+    public string targetText { get; private set; } = "";
+    public string preText { get; private set; } = "";//已有的文本，让打字机可以新加或者可以直接加入
+    private int preTextLength = 0;
+
+    public string fullTargetText => preText + targetText;//应该构建的完整文本
+
+    public enum BuildMethod { instant, typewriter, fade}
+    public BuildMethod buildMethod = BuildMethod.typewriter;
+
+    public Color textColor { get { return tmpro.color; } set { tmpro.color = value; } }
+
+    public float speed { get { return BASESPEED * speedMultiplier; } set { speedMultiplier = value; } }
+    private const float BASESPEED = 1;
+    private float speedMultiplier = 1;
+
+    //点击事件加快文字速度
+
+    //每帧显示的字符数量来达到加快的效果
+    public int charactersPerCycle { get { return speed <= 2f ? characterMultiplier : speed <= 2.5f ? characterMultiplier * 2 : characterMultiplier * 3;  } }
+    private int characterMultiplier = 1;
+
+    public bool hurryUp = false;
 }
