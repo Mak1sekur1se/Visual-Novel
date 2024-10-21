475d7c69efa01da0de55e04e432fde3abff3aff4
WXLing <3127242797@qq.com>
2024-10-21 17:55:45 +0800

    dev:添加UI结构，初始化文件目录，添加部分文本打字机

---
+++ Assets/_Game/Scripts/Core/TextArchitect.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextArchitect : MonoBehaviour
+{
+    [SerializeField] private TMP_Text text; 
+}
